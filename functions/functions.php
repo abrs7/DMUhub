74cd37847da7dc42fe8aba19e8a6b958cd4add37
Abraham Asrat <abrishsambong@gmail.com>
2023-05-23 23:51:32 +0300

    Second Edition

--- functions/functions.php
+++ functions/functions.php
@@ -21,15 +21,65 @@ function insertpost(){
 
 
     if(strlen($content) > 250 ){
-        echo "<script>alert('Please minimize the words!')</script>"
-        echo "<script>window.open('home.php','_self')</script>"    
+        echo "<script>alert('Please minimize the words!')</script>";
+        echo "<script>window.open('home.php','_self')</script>" ;   
     
     }else{
         if(strlen($upload_image)  >= 1 && strlen($content) >= 1 ){
             move_uploaded_file($image_tmp, "imagepost/$upload_image.$random_number");
 
-          $insert = "insert into posts"
+          $insert = "insert into posts (user_id, post_content, upload_image, post_date) values('$user_id', '$content','$upload_image.$random_number', NOW())";
+        
+         $run = mysqli_query($con, $insert);
 
+
+         if($run){
+             echo "<script>alert('Your Post is availble Now!');</script>";
+              
+             echo "<script>window.open('home.php', '_self');</script>";
+   
+            $update = "update users set posts ='yes' where user_id = '$user_id' ";
+            
+            $run_update = mysqli_query($con, $update);
+            }
+        exit();
+        }else{
+            if($upload_image='' && $content == ''){
+            echo "<script>alert('Error occured while uploading it');</script>";
+            echo "<script>window.open('home.php', '_self');</script>";
+        }else{
+            if($content == ''){
+                move_uploaded_file($image_tmp, "imagepost/$upload_image.$random_number");
+            
+                $insert = "insert into posts (user_id, post_content, upload_image, post_date) values('$user_id', 'No','$upload_image.$random_number', NOW())";
+                $run = mysqli_query($con, $insert);
+
+
+                if($run){
+                    echo "<script>alert('Your Post is availble Now!');</script>";
+                     
+                    echo "<script>window.open('home.php', '_self');</script>";
+          
+                   $update = "update users set posts ='yes' where user_id = '$user_id' ";
+                   
+                   $run_update = mysqli_query($con, $update);
+                   }
+               exit();
+            }else{
+                $insert = "insert into posts (user_id, post_content, post_date) values('$user_id', 'No', NOW())";
+                $run = mysqli_query($con, $insert);
+
+
+                if($run){
+                    echo "<script>alert('Your Post is availble Now!');</script>";
+                     
+                    echo "<script>window.open('home.php', '_self');</script>";
+          
+                   $update = "update users set posts ='yes' where user_id = '$user_id' ";
+                   
+                   $run_update = mysqli_query($con, $update);
+            }
+        }
         }
     }
 
@@ -43,7 +93,7 @@ function insertpost(){
 
 
 
-
+}
 
 
 
